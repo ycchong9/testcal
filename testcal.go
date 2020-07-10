2a7aba7505675f5b2b026def7bf336961faef440
ycchong9 <yuechia@smartblock.pro>
2020-07-10 12:13:42 +0800

    added sum function

--- testcal.go
+++ testcal.go
@@ -1,8 +1,17 @@
 package testcal
 
-import "fmt"
+//Sum FUnction
+func Sum(n1 int, n2 int) (int, error) {
+	if n1 > 100 {
+		return 0, &CusError{}
+	}
+	sum := n1 + n2
+	return sum, nil
+}
 
-//Hello FUnction
-func Hello() {
-	fmt.Println("NI HAO")
+//CusError Type
+type CusError struct{}
+
+func (m *CusError) Error() string {
+	return "first number cannot greater than 100"
 }
