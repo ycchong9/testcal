5835abdc5002c7bbb8623cae6d74518ac13b7f42
ycchong9 <yuechia@smartblock.pro>
2020-07-10 12:34:59 +0800

    update function

--- testcal.go
+++ testcal.go
@@ -3,15 +3,24 @@ package testcal
 //Sum FUnction
 func Sum(n1 int, n2 int) (int, error) {
 	if n1 > 100 {
-		return 0, &CusError{}
+		return 0, &CusError{
+			Msg: "first number cannot greater than 100",
+		}
+	}
+	if n2 > 200 {
+		return 0, &CusError{
+			Msg: "second number cannot greater than 200",
+		}
 	}
 	sum := n1 + n2
 	return sum, nil
 }
 
 //CusError Type
-type CusError struct{}
+type CusError struct {
+	Msg string
+}
 
 func (m *CusError) Error() string {
-	return "first number cannot greater than 100"
+	return m.Msg
 }
